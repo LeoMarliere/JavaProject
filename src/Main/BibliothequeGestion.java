605ff46b3dd6fae82675c0e5b6a3e6b3a21078bf
Marliere Léo <leomarliere@hotmail.fr>
2017-11-03 16:36:17 +0100

    Modif constructeur book

--- src/Main/BibliothequeGestion.java
+++ src/Main/BibliothequeGestion.java
@@ -26,22 +26,50 @@ public class BibliothequeGestion {
 	 */
 	public static List<Book> createBookList() {
 		final List<Book> maListe = new ArrayList<>();
+		
+		
+		List<String> auteur1 = new ArrayList<>();
+		auteur1.add("Auteur1");
+		
+		List<String> auteur2 = new ArrayList<>();
+		auteur1.add("Auteur2");
+		
+		List<String> auteur3 = new ArrayList<>();
+		auteur1.add("Auteur3");
+		
+		List<String> auteur4 = new ArrayList<>();
+		auteur1.add("Auteur4");
+		
+		List<String> auteur5 = new ArrayList<>();
+		auteur1.add("Auteur5");
+	
+		List<String> auteur6 = new ArrayList<>();
+		auteur1.add("Auteur6");
+		
+		List<String> auteur7 = new ArrayList<>();
+		auteur1.add("Auteur7");
+		
+		List<String> auteur8 = new ArrayList<>();
+		auteur1.add("Auteur8");
+		
+		List<String> auteur9 = new ArrayList<>();
+		auteur1.add("Auteur9");
+		
+		List<String> auteur10 = new ArrayList<>();
+		auteur1.add("Auteur10");
+		
 
-		final Book livre1 = new Book("1", "AAA", "Livre1", "Auteur1", 4);
-		final Book livre2 = new Book("2", "AAB", "Livre2", "Auteur2", 1);
-		final Book livre3 = new Book("3", "AAC", "Livre3", "Auteur3", 3);
-		final Book livre4 = new Book("4", "AAD", "Livre4", "Auteur4", 2);
-		final Book livre5 = new Book("5", "AAE", "Livre5", "Auteur5", 7);
-		final Book livre6 = new Book("6", "AAF", "Livre6", "Auteur6", 3);
-		final Book livre7 = new Book("7", "AAG", "Livre7", "Auteur7", 5);
-		final Book livre8 = new Book("8", "AAH", "Livre8", "Auteur8", 1);
-		final Book livre9 = new Book("9", "AAI", "Livre9", "Auteur9", 3);
-		final Book livre10 = new Book("10", "AAJ", "Livre10", "Auteur10", 5);
-		final Book livre11 = new Book("11", "AAK", "Livre11", "Auteur11", 4);
-		final Book livre12 = new Book("12", "AAL", "Livre12", "Auteur12", 4);
-		final Book livre13 = new Book("13", "AAM", "Livre13", "Auteur13", 3);
-		final Book livre14 = new Book("14", "AAN", "Livre14", "Auteur14", 2);
-		final Book livre15 = new Book("15", "AAO", "Livre15", "Auteur15", 3);
+		final Book livre1 = new Book("1", "AAA", "Livre1", auteur1 , 4);
+		final Book livre2 = new Book("2", "AAB", "Livre2", auteur2, 1);
+		final Book livre3 = new Book("3", "AAC", "Livre3", auteur3, 3);
+		final Book livre4 = new Book("4", "AAD", "Livre4", auteur4, 2);
+		final Book livre5 = new Book("5", "AAE", "Livre5", auteur5, 7);
+		final Book livre6 = new Book("6", "AAF", "Livre6", auteur6, 3);
+		final Book livre7 = new Book("7", "AAG", "Livre7", auteur7, 5);
+		final Book livre8 = new Book("8", "AAH", "Livre8", auteur8, 1);
+		final Book livre9 = new Book("9", "AAI", "Livre9", auteur9, 3);
+		final Book livre10 = new Book("10", "AAJ", "Livre10", auteur10, 5);
+
 
 		maListe.add(livre1);
 		maListe.add(livre2);
@@ -53,11 +81,7 @@ public class BibliothequeGestion {
 		maListe.add(livre8);
 		maListe.add(livre9);
 		maListe.add(livre10);
-		maListe.add(livre11);
-		maListe.add(livre12);
-		maListe.add(livre13);
-		maListe.add(livre14);
-		maListe.add(livre15);
+
 
 		return maListe;
 	}
