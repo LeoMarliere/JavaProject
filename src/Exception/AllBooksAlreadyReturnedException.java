750ad5ac4a99d01d7012caa4885a189247c7303c
Nopac <captheo@hotmail.fr>
2017-11-03 16:57:10 +0100

    Exception pas fini

---
+++ src/Exception/AllBooksAlreadyReturnedException.java
@@ -0,0 +1,13 @@
+package Exception;
+
+public class AllBooksAlreadyReturnedException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+
+		System.out.println("All books already returned");
+	}
+}
---
+++ src/Exception/BookNotFoundException.java
@@ -0,0 +1,12 @@
+package Exception;
+
+public class BookNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		System.out.println("Book not found");
+	}
+}
