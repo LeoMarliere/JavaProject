17d5c53dab4a6f05643ef60b448935d1515fe4dd
Théo Capon <captheo@hotmail.fr>
2017-11-07 21:51:48 +0100

    Exception

---
+++ src/Exception/LibraryException.java
@@ -0,0 +1,14 @@
+package Exception;
+
+public class LibraryException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public LibraryException(String s){
+		super(s);
+	}
+
+}
