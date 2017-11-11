f056006edafb479ee51e50b8bfc43162c69bf8ce
Haze59240 <mickael.croquet@laposte.net>
2017-11-11 19:23:29 +0100

    Test unitaire 75% de fais, bug sur return book

--- src/Project/Bibliotheque.java
+++ src/Project/Bibliotheque.java
@@ -1,4 +1,5 @@
 package Project;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -6,7 +7,7 @@ import java.util.Optional;
 
 import Exception.LibraryException;
 
-public class Bibliotheque implements IBibliotheque{
+public class Bibliotheque implements IBibliotheque {
 	private List<User> user;
 	private List<Book> book;
 
@@ -19,23 +20,25 @@ public class Bibliotheque implements IBibliotheque{
 	public List<User> getUser() {
 		return user;
 	}
+
 	public void setUser(List<User> user) {
 		this.user = user;
 	}
+
 	public List<Book> getBook() {
 		return book;
 	}
+
 	public void setBook(List<Book> book) {
 		this.book = book;
 	}
 
-
 	public Optional<Book> getBook(String id) {
-		int i=0;
-		Iterator it=book.iterator();
-		while(it.hasNext()){
-			if(book.get(i).getId()==Integer.parseInt(id)){
-				return  Optional.of(book.get(i));
+		int i = 0;
+		final Iterator it = book.iterator();
+		while (it.hasNext()) {
+			if (book.get(i).getId() == Integer.parseInt(id)) {
+				return Optional.of(book.get(i));
 			}
 			i++;
 			it.next();
@@ -44,11 +47,11 @@ public class Bibliotheque implements IBibliotheque{
 	}
 
 	public Optional<String> addBook(String isbn) {
-		int i=0;
-		Iterator<Book> it=book.iterator();
-		while(it.hasNext()){
-			if(book.get(i).getIsbn()==isbn){
-				book.get(i).setNbExemplaire(book.get(i).getNbExemplaire()+1);
+		int i = 0;
+		final Iterator<Book> it = book.iterator();
+		while (it.hasNext()) {
+			if (book.get(i).getIsbn() == isbn) {
+				book.get(i).setNbExemplaire(book.get(i).getNbExemplaire() + 1);
 			}
 			it.next();
 			i++;
@@ -57,19 +60,19 @@ public class Bibliotheque implements IBibliotheque{
 	}
 
 	public void borrowBook(String id, String username) throws LibraryException {
-		int i=0;
-		Iterator<Book> it=book.iterator();
-		int j=0;
-		Iterator<User> it2=user.iterator();
-		while(it.hasNext()){
-			if(book.get(i).getId()==Integer.parseInt(id)){
-				while(it2.hasNext()){
-					if(user.get(j).getUsername()==username && !user.get(j).getBook().contains(book.get(i))){
-						if(book.get(i).getNbExemplaire()<=0){
-							throw new LibraryException("Le livre "+book.get(j).getTitre()+" n'est plus disponible");
+		int i = 0;
+		final Iterator<Book> it = book.iterator();
+		int j = 0;
+		final Iterator<User> it2 = user.iterator();
+		while (it.hasNext()) {
+			if (book.get(i).getId() == Integer.parseInt(id)) {
+				while (it2.hasNext()) {
+					if (user.get(j).getUsername() == username && !user.get(j).getBook().contains(book.get(i))) {
+						if (book.get(i).getNbExemplaire() <= 0) {
+							throw new LibraryException("Le livre " + book.get(j).getTitre() + " n'est plus disponible");
 						}
 						user.get(j).addBook(book.get(i));
-						book.get(i).setNbExemplaire(book.get(i).getNbExemplaire()-1);
+						book.get(i).setNbExemplaire(book.get(i).getNbExemplaire() - 1);
 					}
 					j++;
 					it2.next();
@@ -78,22 +81,22 @@ public class Bibliotheque implements IBibliotheque{
 			i++;
 			it.next();
 		}
-		if(j==0){
+		if (j == 0) {
 			throw new LibraryException("le livre que vous chercher n'est pas dans notre bibliothèque");
 		}
 	}
 
 	public void returnBook(String id, String username) throws LibraryException {
-		int i=0;
-		Iterator<Book> it=book.iterator();
-		int j=0;
-		Iterator<User> it2=user.iterator();
-		while(it.hasNext()){
-			if(book.get(i).getId()==Integer.parseInt(id)){
-				while(it2.hasNext()){
-					if(user.get(j).getUsername()==username && user.get(j).getBook().contains(book.get(i))){
+		int i = 0;
+		final Iterator<Book> it = book.iterator();
+		int j = 0;
+		final Iterator<User> it2 = user.iterator();
+		while (it.hasNext()) {
+			if (book.get(i).getId() == Integer.parseInt(id)) {
+				while (it2.hasNext()) {
+					if (user.get(j).getUsername() == username && user.get(j).getBook().contains(book.get(i))) {
 						user.get(j).removeBook(book.get(i));
-						book.get(i).setNbExemplaire(book.get(i).getNbExemplaire()+1);
+						book.get(i).setNbExemplaire(book.get(i).getNbExemplaire() + 1);
 					}
 					j++;
 					it2.next();
@@ -102,8 +105,9 @@ public class Bibliotheque implements IBibliotheque{
 			i++;
 			it.next();
 		}
-		if(j==0){
-			throw new LibraryException("le livre que vous voulez nous retourner n'est pas référencé dans notre bibliothèque");
+		if (j == 0) {
+			throw new LibraryException(
+					"le livre que vous voulez nous retourner n'est pas référencé dans notre bibliothèque");
 		}
 	}
 
@@ -112,11 +116,12 @@ public class Bibliotheque implements IBibliotheque{
 	}
 
 	public List<Book> searchBooks(String searchTerm) {
-		List <Book> res= new ArrayList<Book>();
-		int i=0;
-		Iterator<Book> it=book.iterator();
-		while(it.hasNext()){
-			if(book.get(i).getAuteur().matches(searchTerm) || book.get(i).getTitre().matches(searchTerm) || book.get(i).getIsbn().matches(searchTerm)){
+		final List<Book> res = new ArrayList<Book>();
+		int i = 0;
+		final Iterator<Book> it = book.iterator();
+		while (it.hasNext()) {
+			if (book.get(i).getAuteur().matches(searchTerm) || book.get(i).getTitre().matches(searchTerm)
+					|| book.get(i).getIsbn().matches(searchTerm)) {
 				res.add((book).get(i));
 			}
 			i++;
--- src/Test/BibliothequeTest.java
+++ src/Test/BibliothequeTest.java
@@ -1,13 +1,132 @@
 package Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import Exception.LibraryException;
-import org.junit.Test;
+import Project.Bibliotheque;
+import Project.Book;
+import Project.User;
+import junit.framework.TestCase;
 
-public class BibliothequeTest {
+public class BibliothequeTest extends TestCase {
 
-	
+	List<Book> listofbooks = new ArrayList();
+	List<User> listofUsers = new ArrayList();
 
+	public void testgetBook() {
+		// what ?
+		final Book booktotest = new Book(4, "123ABC", "Voltaire", "Candide", 1);
+		listofbooks.add(booktotest);
+		final Bibliotheque bibliothequetotest = new Bibliotheque(listofUsers, listofbooks);
+		// when ?
+		final Optional result = Optional.of(bibliothequetotest.getBook("4"));
+		// then ?
+		assertNotNull(result);
+	}
 
+	public void testgsearchBooksByAuteur() {
+		// what ?
+		final Book booktotest = new Book(4, "123ABC", "Voltaire", "Candide", 1);
+		final Book booktotest2 = new Book(5, "456DEF", "Voltaire", "Candide", 1);
+		listofbooks.add(booktotest);
+		listofbooks.add(booktotest2);
+		final Bibliotheque bibliothequetotest = new Bibliotheque(listofUsers, listofbooks);
+		// when ?
+		final List<Book> result = bibliothequetotest.searchBooks("Voltaire");
+		// then ?
+		assertNotNull(result);
+		assertEquals(2, result.size());
+	}
 
+	public void testgsearchBooksByIsbn() {
+		// what ?
+		final Book booktotest = new Book(4, "123ABC", "Voltaire", "Candide", 1);
+		final Book booktotest2 = new Book(5, "456DEF", "Voltaire", "Zadig", 1);
+		listofbooks.add(booktotest);
+		listofbooks.add(booktotest2);
+		final Bibliotheque bibliothequetotest = new Bibliotheque(listofUsers, listofbooks);
+		// when ?
+		final List<Book> result1 = bibliothequetotest.searchBooks("123ABC");
+		final List<Book> result2 = bibliothequetotest.searchBooks("456DEF");
+		// then ?
+		assertNotNull(result1);
+		assertNotNull(result2);
+		assertEquals(1, result1.size());
+		assertEquals(1, result2.size());
+	}
+
+	public void testgsearchBooksByTitre() {
+		// what ?
+		final Book booktotest = new Book(4, "123ABC", "Voltaire", "Candide", 1);
+		final Book booktotest2 = new Book(5, "456DEF", "Voltaire", "Zadig", 1);
+		listofbooks.add(booktotest);
+		listofbooks.add(booktotest2);
+		final Bibliotheque bibliothequetotest = new Bibliotheque(listofUsers, listofbooks);
+		// when ?
+		final List<Book> result1 = bibliothequetotest.searchBooks("Candide");
+		final List<Book> result2 = bibliothequetotest.searchBooks("Zadig");
+		// then ?
+		assertNotNull(result1);
+		assertNotNull(result2);
+		assertEquals(1, result1.size());
+		assertEquals(1, result2.size());
+	}
+
+	public void testBorrowBook() throws LibraryException {
+		// what ?
+		final Book booktotest = new Book(4, "123ABC", "Voltaire", "Candide", 1);
+		final User usertotest = new User(0, "Croquet");
+		listofUsers.add(usertotest);
+		listofbooks.add(booktotest);
+		final Bibliotheque bibliothequetotest = new Bibliotheque(listofUsers, listofbooks);
+		// when ?
+		bibliothequetotest.borrowBook("4", "Croquet");
+		// then ?
+		final List<Book> bibliothequelistofbooks = bibliothequetotest.getBooks();
+		final List<User> bibliothequelistofuser = bibliothequetotest.getUser();
+		final Book thebookbeanborrow = bibliothequelistofbooks.get(0);
+		final User theuserwhoborrowbook = bibliothequelistofuser.get(0);
+		final int numberofbookborrowbyuser = theuserwhoborrowbook.getBook().size();
+		final int numberofbookinbibliotheque = thebookbeanborrow.getNbExemplaire();
+		assertEquals(0, numberofbookinbibliotheque);
+		assertEquals(1, numberofbookborrowbyuser);
+	}
+
+	public void testReturnBook() throws LibraryException {
+		// what ?
+		final Book booktotest = new Book(4, "123ABC", "Voltaire", "Candide", 1);
+		final User usertotest = new User(0, "Croquet");
+		listofbooks.add(booktotest);
+		usertotest.setBook(listofbooks);
+		listofUsers.add(usertotest);
+
+		final Bibliotheque bibliothequetotest = new Bibliotheque(listofUsers, listofbooks);
+
+		// when ?
+		bibliothequetotest.returnBook("4", "Croquet");
+
+		// then ?
+		final User userwithbook = bibliothequetotest.getUser().get(0);
+		final List<Book> Userlistofbook = userwithbook.getBook();
+		final int numberofthesamebook = Userlistofbook.get(0).getNbExemplaire();
+		assertEquals(1, numberofthesamebook);
+	}
+
+	public void testAddBook() throws LibraryException {
+		// what ?
+		final Book booktotest = new Book(4, "123ABC", "Voltaire", "Candide", 1);
+		final User usertotest = new User(0, "Croquet");
+		listofUsers.add(usertotest);
+		listofbooks.add(booktotest);
+		final Bibliotheque bibliothequetotest = new Bibliotheque(listofUsers, listofbooks);
+		// when ?
+		bibliothequetotest.addBook("123ABC");
+		// then ?
+		final List<Book> bibliothequelistofbooks = bibliothequetotest.getBooks();
+		final Book thebookadded = bibliothequelistofbooks.get(0);
+		final int numberofbookinbibliotheque = thebookadded.getNbExemplaire();
+		assertEquals(2, numberofbookinbibliotheque);
+	}
 }
